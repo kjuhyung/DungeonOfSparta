e306554c4def043d7b1fecfe54f66b5b02d220c1
kim ju hyung <kim52116@naver.com>
2023-08-22 16:50:31 +0900

    상점 추가 中
    myItems , shopItems List
    DisplayShop() 메서드

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
-﻿using System.Reflection.Metadata.Ecma335;
-
+﻿
 namespace DungeonOfSparta
 {
     // 게임 시작 화면
@@ -21,7 +20,8 @@ namespace DungeonOfSparta
     internal class Program
     {
         private static Character player;
-        private static List<Items> items;
+        private static List<Items> myitems;
+        private static List<Items> shopitems;
 
         public class Character
         {
@@ -51,6 +51,7 @@ namespace DungeonOfSparta
         {
             // 아이템 클래스 만들기
             public bool IsEquipped { get; set; }
+            public bool IsHave { get; set; } 
             public string Name { get; }
             public string Type { get; }
             public string Info { get; }
@@ -75,13 +76,18 @@ namespace DungeonOfSparta
             // 캐릭터 정보 세팅
             player = new Character("Leonidas", "전사", 1, 10, 5, 100, 1500);
             // 아이템 정보 세팅
-            items = new List<Items>();
-            items.Add(new Items("나무 검", "무기", " 검의 형상으로 깎은 나무... ", 500, 3, 0));
-            // items.Add(new Items("돌 검", "무기", " 단순하지만 위력적 ", 1000, 5, 0));
-            // items.Add(new Items("철 검", "무기", " 충분한 공격력을 갖춘 보편적인 장비 ", 1500, 10, 0));
-            items.Add(new Items("천 갑옷", "방어구", " 천으로 만든 기본적인 갑옷 ", 500, 0, 3));
-            // items.Add(new Items("가죽 갑옷", "방어구", " 가볍지만 유연한 방어를 제공 ", 1000, 0, 5));
-            // items.Add(new Items("사슬 갑옷", "방어구", " 기동성과 효과를 적절히 갖춘 보편적인 장비 ", 1500, 0, 10));
+            myitems = new List<Items>();
+            myitems.Add(new Items("나무 검", "무기", " 검의 형상으로 깎은 나무... ", 500, 3, 0));            
+            myitems.Add(new Items("천 갑옷", "방어구", " 천으로 만든 기본적인 갑옷 ", 500, 0, 3));
+            
+
+            shopitems = new List<Items>();
+            shopitems.Add(new Items("돌 검", "무기", " 단순하지만 위력적 ", 1000, 5, 0));
+            shopitems.Add(new Items("철 검", "무기", " 충분한 공격력을 갖춘 보편적인 장비 ", 1500, 10, 0));
+            shopitems.Add(new Items("가죽 갑옷", "방어구", " 가볍지만 유연한 방어를 제공 ", 1000, 0, 5));
+            shopitems.Add(new Items("사슬 갑옷", "방어구", " 기동성과 효과를 적절히 갖춘 보편적인 장비 ", 1500, 0, 10));
+
+
         }
 
         static void DisplayGameStart()
@@ -97,13 +103,14 @@ namespace DungeonOfSparta
             Console.WriteLine();
             Console.WriteLine(" 1. 상태 보기 ");
             Console.WriteLine(" 2. 가방 보기 ");
+            Console.WriteLine(" 3. 상점 보기 ");
             Console.WriteLine();
             Console.WriteLine(" 원하시는 행동을 입력해주세요! ");
 
             // 입력 받아오기
-            int input = CheckUserInput(1, 2);
+            int input = CheckUserInput(1, 3);
             switch (input)
-            {
+            {                
                 case 1:
                     // 상태 보기
                     DisplayMyInfo();
@@ -111,6 +118,10 @@ namespace DungeonOfSparta
                 case 2:
                     // 가방 보기
                     DisplayInventory();
+                    break;
+                case 3:
+                    // 상점 보기
+                    DisplayShop();
                     break;
             }
         }
@@ -122,20 +133,19 @@ namespace DungeonOfSparta
             int bonusAtk= 0;
             int bonusDef= 0;
 
-
-            for (int i = 0; i < items.Count; i++)
+            for (int i = 0; i < myitems.Count; i++)
             {
-                if (items[i].IsEquipped)
+                if (myitems[i].IsEquipped)
                 {
-                    if (items[i].Type == "무기")
+                    if (myitems[i].Type == "무기")
                     {
-                        bonusAtk += items[i].Atk;
+                        bonusAtk += myitems[i].Atk;
                         playerAtk += ($" (+{bonusAtk})");
                     }
 
-                    else if (items[i].Type == "방어구")
+                    else if (myitems[i].Type == "방어구")
                     {
-                        bonusDef += items[i].Def;
+                        bonusDef += myitems[i].Def;
                         playerDef += ($" (+{bonusDef})");
                     }
                 }
@@ -184,13 +194,55 @@ namespace DungeonOfSparta
             Console.WriteLine(" [아이템 목록] ");            
             Console.ResetColor();
             Console.WriteLine();
-            for (int i = 0; i<items.Count; i++)
+            for (int i = 0; i<myitems.Count; i++)
             {
                 Console.Write(" - ");
-                ItemText(items[i]);
+                ItemText(myitems[i]);
             }           
             Console.WriteLine();
             Console.WriteLine(" 1. 장착 관리 ");
+            Console.WriteLine(" 0. 나가기 ");
+            Console.WriteLine();
+            Console.WriteLine(" 원하시는 행동을 입력해주세요! ");
+
+            int input = CheckUserInput(0, 1);
+            switch (input)
+            {
+                case 0:
+                    DisplayGameStart();
+                    break;
+                case 1:
+                    DisplayEquipment();
+                    break;
+            }
+        }
+        static void DisplayShop()
+        {
+            // 인벤토리 창 만들기
+            Console.Clear();
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(" ~ 상점 ~ ");
+            Console.ResetColor();
+            Console.WriteLine();
+            Console.WriteLine(" 아이템을 구매/판매할 수 있습니다. ");
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            Console.WriteLine($" [보유 골드] \t{player.Gold} G ");
+            Console.ResetColor();
+            Console.WriteLine();            
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine(" [아이템 목록] ");
+            Console.ResetColor();
+            Console.WriteLine();
+            for (int i = 0; i < shopitems.Count; i++)
+            {
+                Console.Write(" - ");
+                ItemText(shopitems[i]);
+            }
+            Console.WriteLine();
+            Console.WriteLine(" 1. 구매하기 ");
+            Console.WriteLine(" 2. 판매하기 ");
             Console.WriteLine(" 0. 나가기 ");
             Console.WriteLine();
             Console.WriteLine(" 원하시는 행동을 입력해주세요! ");
@@ -224,14 +276,14 @@ namespace DungeonOfSparta
             Console.WriteLine(" [아이템 목록] ");
             Console.ResetColor();
             Console.WriteLine();
-            for (int i = 0; i < items.Count; i++)
+            for (int i = 0; i < myitems.Count; i++)
             {
                 Console.Write($" {i + 1}.");
-                ItemText(items[i]);
+                ItemText(myitems[i]);
             } 
             Console.WriteLine();
             
-            for (int i= 1; i <= items.Count; i++)
+            for (int i= 1; i <= myitems.Count; i++)
             {
                 Console.WriteLine($" {i}. 장착/해제하기 ");
             }
@@ -241,11 +293,11 @@ namespace DungeonOfSparta
             Console.WriteLine();
 
             // 번호를 선택해서 장착하기
-            int input = CheckUserInput(0, items.Count);
-            if (input > 0 && input <=items.Count)
+            int input = CheckUserInput(0, myitems.Count);
+            if (input > 0 && input <=myitems.Count)
             {
                 int itemIndex = input - 1;
-                Items selectedItems = items[itemIndex];
+                Items selectedItems = myitems[itemIndex];
 
                 selectedItems.IsEquipped = !selectedItems.IsEquipped;
                 DisplayEquipment();
@@ -268,7 +320,7 @@ namespace DungeonOfSparta
                 }
                 else if (!item.IsEquipped)
                 {
-                    Console.WriteLine($"{item.Name}\t | 공격력 : {item.Atk}\t |{item.Info}\t | {item.Price}G");
+                    Console.WriteLine($"{item.Name}\t | 공격력 : {item.Atk}\t |{item.Info}\t| {item.Price}G");
                 }
             }
 
@@ -284,10 +336,7 @@ namespace DungeonOfSparta
                     Console.WriteLine($"{item.Name}\t | 방어력 : {item.Def}\t |{item.Info}\t | {item.Price}G");
                 }                  
             }                  
-        }
-        
-
-        
+        }      
         static int CheckUserInput(int min,int max)
         {            
             while (true) // 무한루프
